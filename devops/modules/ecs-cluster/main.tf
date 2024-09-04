7d202830a7b6ce4499c77fb74719851473e0fdac
Jhonatan Rojas <jrojas02@gmail.com>
2024-09-04 06:19:30 -0500

    Update

--- devops/modules/ecs-cluster/main.tf
+++ devops/modules/ecs-cluster/main.tf
@@ -44,15 +44,3 @@ resource "aws_iam_role" "ecs_task_role" {
     ]
   })
 }
-
-output "cluster_id" {
-  value = aws_ecs_cluster.this.id
-}
-
-output "ecs_task_execution_role_arn" {
-  value = aws_iam_role.ecs_task_execution_role.arn
-}
-
-output "ecs_task_role_arn" {
-  value = aws_iam_role.ecs_task_role.arn
-}
