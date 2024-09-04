ab4b851218f9f8b3955a4eb036e7de4803b3e49e
Jhonatan Rojas <jrojas02@gmail.com>
2024-09-04 05:18:46 -0500

    Update

--- devops/terraform.tfvars
+++ devops/terraform.tfvars
@@ -1,3 +1,1 @@
-s3_bucket       = "my-terraform-state-bucket"
 region          = "us-east-1"
-cluster_name    = "my-cluster"
--- devops/variables.tf
+++ devops/variables.tf
@@ -3,14 +3,3 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "s3_bucket" {
-  description = "S3 bucket for Terraform state"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "Name of the ECS cluster"
-  type        = string
-  default     = "my-cluster"
-}
